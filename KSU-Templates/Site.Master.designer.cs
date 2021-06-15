275d9cc1f2645605601a06f6adc0a82b7e5a8f8f
hadeel10 <61603080+hadeel10@users.noreply.github.com>
2021-06-15 19:06:09 +0300

    link about page

--- KSU-Templates/Site.Master.designer.cs
+++ KSU-Templates/Site.Master.designer.cs
@@ -38,5 +38,14 @@ namespace KSU_Templates {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
+        
+        /// <summary>
+        /// MainContent control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder MainContent;
     }
 }
